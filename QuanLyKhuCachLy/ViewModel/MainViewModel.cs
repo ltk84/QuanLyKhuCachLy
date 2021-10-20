7e4a516af71d6a63cd48be751e5ef0349dc3dfbe
TungTLT <tranlethanhtung554@gmail.com>
2021-10-20 13:49:29 +0700

    setup mvvm model for project

---
+++ QuanLyKhuCachLy/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyKhuCachLy.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("Mot giac mo em dem danh cho nhau!");
+        }
+    }
+}
