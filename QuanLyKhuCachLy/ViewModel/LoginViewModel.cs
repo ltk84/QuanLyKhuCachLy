6a0d7ebf647e331414b076ab0670f7d92e7ba748
TungTLT <tranlethanhtung554@gmail.com>
2021-10-21 16:23:29 +0700

    fake code UI loginWindow, init login flow

---
+++ QuanLyKhuCachLy/ViewModel/LoginViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyKhuCachLy.ViewModel
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public bool  isLogin { get; set; }
+
+        private string _username;
+        public string username { get => _username; set { _username = value; OnPropertyChanged(); } }
+
+        private string _password;
+        public string password { get => _password; set { _password = value; OnPropertyChanged(); } }
+
+        public ICommand loginCommand { get; set; }
+        public ICommand closeCommand { get; set; }
+        public ICommand passwordChangedCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            isLogin = false;
+            username = "";
+            password = "";
+
+            loginCommand = new RelayCommand<LoginWindow>((p) => { return true; }, (p) =>
+            {
+                login(p);
+            });
+
+            closeCommand = new RelayCommand<LoginWindow>((p) => { return true; }, (p) =>
+            {
+                p.Close();
+            });
+
+            passwordChangedCommand = new RelayCommand<PasswordBox>((p) => { return true; }, (p) =>
+            {
+                
+            });
+        }
+
+        void login(LoginWindow p) {
+            isLogin = true;
+            p.Close();
+        }
+    }
+}
--- QuanLyKhuCachLy/ViewModel/MainViewModel.cs
+++ QuanLyKhuCachLy/ViewModel/MainViewModel.cs
@@ -4,14 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace QuanLyKhuCachLy.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+        public bool isLoaded { get; set; }
+        public ICommand loadedCommand { get; set; }
+
         public MainViewModel()
         {
-            MessageBox.Show("Mot giac mo em dem danh cho nhau!");
+            isLoaded = false;
+            loadedCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                loadLoginScreen(p);
+            });
+            
+        }
+
+        void loadLoginScreen(Window p)
+        {
+            isLoaded = true;
+            if (p == null) return;
+            p.Hide();
+
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+
+            if (loginWindow.DataContext == null) return;
+            var loginVM = loginWindow.DataContext as LoginViewModel;
+
+            if (loginVM.isLogin) {
+                p.Show();
+            }
+            else {
+                p.Close();
+            }
+
         }
     }
 }
