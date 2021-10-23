b8ebfddd96272e8acf5a1ee168b20cb275a1ac5b
Lam <lamtk.84@gmail.com>
2021-10-23 11:56:09 +0700

    Add Authentication Screen

--- QuanLyKhuCachLy/ViewModel/MainViewModel.cs
+++ QuanLyKhuCachLy/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,51 @@ namespace QuanLyKhuCachLy.ViewModel
     {
         public MainViewModel()
         {
-            MessageBox.Show("Mot giac mo em dem danh cho nhau!");
+            Items1 = CreateData();
+
+        }
+
+        private static ObservableCollection<SelectableViewModel> CreateData()
+        {
+            return new ObservableCollection<SelectableViewModel>
+            {
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                }
+            };
+        }
+
+        public ObservableCollection<SelectableViewModel> Items1 { get; }
+
+
+        public class SelectableViewModel : BaseViewModel
+        {
+            private char code;
+            private string name;
+            private string description;
+            private string food;
+
+
+            public char Code { get => code; set => code = value; }
+            public string Name { get => name; set => name = value; }
+            public string Description { get => description; set => description = value; }
+            public string Food { get => food; set => food = value; }
         }
     }
 }
