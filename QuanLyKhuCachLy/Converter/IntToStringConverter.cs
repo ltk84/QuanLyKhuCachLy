32ecbd56802214eeed5cb0eb02390f364fad0268
TungTLT <tranlethanhtung554@gmail.com>
2021-11-07 18:28:44 +0700

    fix validation rule stop binding last value

---
+++ QuanLyKhuCachLy/Converter/IntToStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace QuanLyKhuCachLy.Converter
+{
+    public class IntToStringConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int price = (int)value;
+            return price.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string text = (string)value;
+            int price;
+            if (!int.TryParse(text, out price))
+            {
+
+                return 0; //Some default value
+            }
+
+            return price;
+        }
+    }
+}
--- QuanLyKhuCachLy/ValidationRules/RequiredField.cs
+++ QuanLyKhuCachLy/ValidationRules/RequiredField.cs
@@ -8,19 +8,49 @@ namespace QuanLyKhuCachLy.ValidationRules
 {
     public class RequiredField : ValidationRule
     {
-
-        public RequiredField()
+        public RequiredField() : base(ValidationStep.CommittedValue, true)
         {
         }
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            if (value is Severity)
+            //if (value is System.Windows.Data.BindingExpression)
+            //{
+            //    var binding = value as BindingExpression;
+            //    ComboBox comboBox = binding.Target as ComboBox;
+            //    if (comboBox == null) return null;
+            //    return comboBox.SelectedItem == null ? new ValidationResult(false, $"Thông tin này là bắt buộc.") : ValidationResult.ValidResult;
+            //}
+
+            var expression = value as BindingExpression;
+            if (expression != null)
             {
-                Severity severity = value as Severity;
-                return String.IsNullOrWhiteSpace(severity.level)
-                    ? new ValidationResult(false, $"Thông tin này là bắt buộc.")
-                    : ValidationResult.ValidResult;
+                var sourceItem = expression.DataItem;
+                if (sourceItem != null)
+                {
+                    var propertyName = expression.ParentBinding != null && expression.ParentBinding.Path != null ? expression.ParentBinding.Path.Path : null;
+                    var sourceValue = sourceItem.GetType().GetProperty(propertyName).GetValue(sourceItem, null);
+                    var propertyType = sourceItem.GetType().GetProperty(propertyName).PropertyType;
+
+                    if (propertyType.Name == "Int32")
+                    {
+                        return Int32.Parse(sourceValue.ToString()) <= 0 ? new ValidationResult(false, $"Thông tin này là bắt buộc.") : ValidationResult.ValidResult;
+                    }
+                    else if (propertyType.Name == "String")
+                    {
+                        return string.IsNullOrWhiteSpace((string)sourceValue) ? new ValidationResult(false, $"Thông tin này là bắt buộc.") : ValidationResult.ValidResult;
+
+                    }
+                    else if (propertyType.Name == "Severity")
+                    {
+
+                        return sourceValue == null ? new ValidationResult(false, $"Thông tin này là bắt buộc.") : ValidationResult.ValidResult;
+                    }
+
+                }
             }
+
+
+
 
             if (value is DateTime)
             {
