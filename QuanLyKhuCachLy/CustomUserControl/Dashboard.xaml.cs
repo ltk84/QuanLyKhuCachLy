1947e477e8fae752dabd68c574e4bdb5b0a56e2f
Lam <lamtk.84@gmail.com>
2021-11-17 09:17:44 +0700

    update dashboard context menu

--- QuanLyKhuCachLy/CustomUserControl/Dashboard.xaml.cs
+++ QuanLyKhuCachLy/CustomUserControl/Dashboard.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace QuanLyKhuCachLy.CustomUserControl
+﻿using System.Windows.Controls;
+
+namespace QuanLyKhuCachLy.CustomUserControl
 {
     /// <summary>
     /// Interaction logic for Dashboard.xaml
@@ -8,6 +10,22 @@
         public Dashboard()
         {
             InitializeComponent();
+        }
+        private void Button_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button == null) return;
+            button.ContextMenu.DataContext = button.DataContext;
+            button.ContextMenu.IsOpen = true;
+            e.Handled = true;
+        }
+        private void Button_PreviewMouseDown1(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button == null) return;
+            button.ContextMenu.DataContext = button.DataContext;
+            button.ContextMenu.IsOpen = true;
+            e.Handled = true;
         }
     }
 }
