017924bb7dce1bcebd520451ac545eb8f127d76a
Phuc-HuynhTrong <phuchuynh020601@gmail.com>
2021-11-22 19:19:19 +0700

    export list QuarantinedPerson to excel

--- QuanLyKhuCachLy/ViewModel/QuarantinePersonViewModel.cs
+++ QuanLyKhuCachLy/ViewModel/QuarantinePersonViewModel.cs
@@ -13,13 +13,13 @@ using Microsoft.Win32;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Windows.Media;
 using System.Threading.Tasks;
-
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
 using System.Threading;
+
 namespace QuanLyKhuCachLy.ViewModel
 {
     public class QuarantinePersonViewModel : BaseViewModel
@@ -747,7 +747,7 @@ namespace QuanLyKhuCachLy.ViewModel
         public ICommand CompleteQuarantinePersonCommand { get; set; }
         public ICommand RefeshCommand { get; set; }
 
-
+        public ICommand ToExportExcel { get; set; }
 
         #endregion
 
@@ -891,6 +891,14 @@ namespace QuanLyKhuCachLy.ViewModel
                 Tab3 = Visibility.Hidden;
                 Tab4 = Visibility.Hidden;
                 TabPosition = $"{TabIndex}/4";
+            });
+
+            ToExportExcel = new RelayCommand<Window>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                ExportExcel();
             });
 
 
@@ -2351,6 +2359,94 @@ namespace QuanLyKhuCachLy.ViewModel
                 MessageBox.Show("No data found.");
             }
 
+        }
+        void ExportExcel()
+        {
+            int count = PeopleListView.Length;
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            app.Visible = true;
+            app.WindowState = Microsoft.Office.Interop.Excel.XlWindowState.xlMaximized;
+            Microsoft.Office.Interop.Excel.Workbook file = app.Workbooks.Add(Microsoft.Office.Interop.Excel.XlWBATemplate.xlWBATWorksheet);
+            Microsoft.Office.Interop.Excel.Worksheet sheet = file.Worksheets[1];
+            sheet.Columns[1].ColumnWidth = 5;
+            sheet.Columns[2].ColumnWidth = 25;
+            sheet.Columns[3].ColumnWidth = 12;
+            sheet.Columns[4].ColumnWidth = 9;
+            sheet.Columns[5].ColumnWidth = 50;
+            sheet.Columns[6].ColumnWidth = 12;
+            sheet.Columns[7].ColumnWidth = 12;
+            sheet.Columns[8].ColumnWidth = 10;
+            sheet.Columns[9].ColumnWidth = 12;
+            sheet.Columns[10].ColumnWidth = 5;
+            sheet.Columns[11].ColumnWidth = 12;
+            sheet.Columns[12].ColumnWidth = 12;
+            sheet.Columns[13].ColumnWidth = 10;
+            sheet.Columns[14].ColumnWidth = 7;
+            sheet.Columns[15].ColumnWidth = 15;
+            sheet.Columns[16].ColumnWidth = 15;
+            sheet.Range["A1"].Value = "STT";
+            sheet.Range["B1"].Value = "Họ và tên";
+            sheet.Range["C1"].Value = "Ngày sinh";
+            sheet.Range["D1"].Value = "Giới tính";
+            sheet.Range["E1"].Value = "Địa chỉ";
+            sheet.Range["F1"].Value = "MaBH";
+            sheet.Range["G1"].Value = "CMND/CCCD";
+            sheet.Range["H1"].Value = "Quốc tịch";
+            sheet.Range["I1"].Value = "SĐT";
+            sheet.Range["J1"].Value = "Nhóm đối tượng";
+            sheet.Range["K1"].Value = "Ngày đến";
+            sheet.Range["L1"].Value = "Ngày hoàn thành";
+            sheet.Range["M1"].Value = "Số ngày\ncách ly";
+            sheet.Range["N1"].Value = "Phòng";
+            sheet.Range["O1"].Value = "Đã hoàn thành\ncách ly";
+            sheet.Range["P1"].Value = "Số mũi vaccine\nđã tiêm";
+
+            for (int i = 2; i <= count+1; i++)
+            {
+                int addressID = PeopleListView[i - 2].addressID.Value;
+                Address address = DataProvider.ins.db.Addresses.Where(x => x.id == addressID).FirstOrDefault();
+                String personAddress = "";
+                if (address.apartmentNumber != null)
+                    personAddress += address.apartmentNumber.ToString();
+                if (address.streetName != null)
+                    personAddress += " " + address.streetName.ToString();
+                if (address.ward != null)
+                    personAddress += ", " + address.ward.ToString();
+                if (address.district != null)
+                    personAddress += ", " + address.district.ToString();
+                if (address.province != null)
+                    personAddress += ", " + address.province.ToString();
+                Severity severity = new Severity(); 
+                if (PeopleListView[i - 2].levelID != null)
+                {
+                    int severityID = PeopleListView[i - 2].levelID.Value;
+                    severity = DataProvider.ins.db.Severities.Where(x => x.id == severityID).FirstOrDefault();
+                }
+                int personId = PeopleListView[i - 2].id; ;
+                int countInjectionRecord = DataProvider.ins.db.InjectionRecords.Where(x => x.quarantinePersonID == personId).Count();
+                QuanLyKhuCachLy.Model.QuarantineRoom room = new Model.QuarantineRoom();
+                if (PeopleListView[i - 2].roomID != null)
+                {
+                    int roomID = PeopleListView[i - 2].roomID.Value;
+                    room = DataProvider.ins.db.QuarantineRooms.Where(x => x.id == roomID).FirstOrDefault();
+                }
+                sheet.Range["A" + i.ToString()].Value = (i-1).ToString();
+                sheet.Range["B" + i.ToString()].Value = PeopleListView[i-2].name;
+                sheet.Range["C" + i.ToString()].Value = PeopleListView[i - 2].dateOfBirth;
+                sheet.Range["D" + i.ToString()].Value = PeopleListView[i - 2].sex;            
+                sheet.Range["E" + i.ToString()].Value = personAddress;
+                sheet.Range["F" + i.ToString()].Value = PeopleListView[i - 2].healthInsuranceID;
+                sheet.Range["G" + i.ToString()].Value = PeopleListView[i - 2].citizenID;
+                sheet.Range["H" + i.ToString()].Value = PeopleListView[i - 2].nationality;
+                sheet.Range["I" + i.ToString()].Value = PeopleListView[i - 2].phoneNumber;
+                sheet.Range["J" + i.ToString()].Value = severity.description != null ? severity.description : "";
+                sheet.Range["K" + i.ToString()].Value = PeopleListView[i - 2].arrivedDate;
+                sheet.Range["L" + i.ToString()].Value = PeopleListView[i - 2].leaveDate;
+                sheet.Range["M" + i.ToString()].Value = PeopleListView[i - 2].quarantineDays;
+                sheet.Range["N" + i.ToString()].Value = PeopleListView[i - 2].roomID != null?room.displayName:"";
+                sheet.Range["O" + i.ToString()].Value = PeopleListView[i - 2].completeQuarantine== true ? "X":"";
+                sheet.Range["P" + i.ToString()].Value = countInjectionRecord;
+            }
         }
         #endregion
     }
